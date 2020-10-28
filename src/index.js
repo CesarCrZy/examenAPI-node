3ca01d6d4262bef67eb0c1ec17510e9eb79888bd
Cesar <elmariachicrazy@outlook.com>
2020-10-27 23:16:03 -0600

    Inicio

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://apirest-examen.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
